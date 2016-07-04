f21bd17ec1aba5e8798490731dea1748725c4ec2
antonreznik <anton.reznik1982@gmail.com>
2016-07-05 00:28:02 +0300

    Start to add modals in admin

--- Shop/CarWashes/scripts/Admin/CategoryAdmin.js
+++ Shop/CarWashes/scripts/Admin/CategoryAdmin.js
@@ -9,7 +9,23 @@
             })
         });
     }
+
+    addEventListenerOnCreateCategory() {
+        document.getElementById("add_category").addEventListener("click", function () {
+            $.ajax({
+                url: "/Category/CreateCategory",
+                success: function (result) {
+                    $("#modal_content").html(result);
+                }
+            });
+
+            $('#modal_window').openModal();
+        });
+    }
 };
 
 let categoryAdmin = new WorkWithCategory();
 categoryAdmin.addEventListenerOnEditButtons();
+categoryAdmin.addEventListenerOnCreateCategory();
+
+
