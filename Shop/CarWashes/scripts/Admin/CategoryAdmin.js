6fdb122268a4070bb36f3fc744afd4b1d1f64073
antonreznik <anton.reznik1982@gmail.com>
2016-07-06 21:37:40 +0300

    Making edit category modal

--- Shop/CarWashes/scripts/Admin/CategoryAdmin.js
+++ Shop/CarWashes/scripts/Admin/CategoryAdmin.js
@@ -1,15 +1,6 @@
 ﻿class WorkWithCategory {
     constructor() { };
 
-    addEventListenerOnEditButtons() {
-        let editButtons = document.querySelectorAll("button[value='editButton']");
-        editButtons.forEach(function (button) {
-            button.addEventListener("click", function () {
-                alert("hello");
-            })
-        });
-    };
-
     addEventListenerOnCreateCategory() {
         document.getElementById("add_category").addEventListener("click", function () {
             $.ajax({
@@ -34,11 +25,32 @@
             });
             $('#modal_window').width("35%").openModal();
         });
-    }; 
+    };
+    addEventListenerOnEditCategory() {
+        document.getElementById("edit_category").addEventListener("click", function () {
+            $.ajax({
+                url: "/Category/EditCategory",
+                success: function (result) {
+                    $("#modal_content").html(result);
+
+                    let editButtons = document.querySelectorAll("button[value='editButton']");
+                    editButtons.forEach(function (button) {
+                        button.addEventListener("click", function (event) {
+                            let input = event.currentTarget.dataset.editbtn;
+                            let field = document.querySelector(`input[data-input="${input}"]`);
+                            field.removeAttribute("disabled");
+                            event.currentTarget.className += "disabled";
+                        })
+                    });
+                }
+            });
+            $('#modal_window').width("65%").openModal();
+        });
+    };
 };
 
 let categoryAdmin = new WorkWithCategory();
-categoryAdmin.addEventListenerOnEditButtons();
 categoryAdmin.addEventListenerOnCreateCategory();
+categoryAdmin.addEventListenerOnEditCategory();
 
 
