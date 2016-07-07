6a48e5133d1ea2853ecc1146d104a91c978bca48
Anton Reznik <anton.reznik@valtech.com>
2016-07-07 10:30:44 +0300

    Add edit in modal

--- Shop/CarWashes/scripts/Admin/CategoryAdmin.js
+++ Shop/CarWashes/scripts/Admin/CategoryAdmin.js
@@ -1,5 +1,7 @@
 ﻿class WorkWithCategory {
-    constructor() { };
+    constructor() {
+
+    };
 
     addEventListenerOnCreateCategory() {
         document.getElementById("add_category").addEventListener("click", function () {
@@ -27,20 +29,49 @@
         });
     };
     addEventListenerOnEditCategory() {
+        let editButtonListener = function (event) {
+            let editButton = event.currentTarget;
+            let inputDataAttr = editButton.dataset.editbtn;
+            let input = document.querySelector(`input[data-input="${inputDataAttr}"]`);
+            let saveButton = document.querySelector(`button[data-savebtn="${inputDataAttr}"]`);
+
+            input.removeAttribute("disabled");
+            $(editButton).addClass("disabled");
+            $(saveButton).removeClass("disabled");
+
+            editButton.removeEventListener("click", editButtonListener);
+            saveButton.addEventListener("click", saveButtonListener);
+        }
+        let saveButtonListener = function (event) {
+            let saveButton = event.currentTarget;
+            let inputDataAttr = saveButton.dataset.savebtn;
+            let input = document.querySelector(`input[data-input="${inputDataAttr}"]`);
+            let editButton = document.querySelector(`button[data-editbtn="${inputDataAttr}"]`);
+
+            input.setAttribute("disabled","disabled");
+            $(editButton).removeClass("disabled");
+            $(saveButton).addClass("disabled");
+
+            saveButton.removeEventListener("click", saveButtonListener);
+            editButton.addEventListener("click", editButtonListener)
+            alert("save");
+            $.ajax({
+                url: "/Category/CreateCategory",
+                method: "POST",
+                data: { "Id": inputDataAttr, "Name": input.value }
+            })
+        }
+
         document.getElementById("edit_category").addEventListener("click", function () {
             $.ajax({
                 url: "/Category/EditCategory",
                 success: function (result) {
                     $("#modal_content").html(result);
 
+                    //add event listener on every edit button
                     let editButtons = document.querySelectorAll("button[value='editButton']");
                     editButtons.forEach(function (button) {
-                        button.addEventListener("click", function (event) {
-                            let input = event.currentTarget.dataset.editbtn;
-                            let field = document.querySelector(`input[data-input="${input}"]`);
-                            field.removeAttribute("disabled");
-                            event.currentTarget.className += "disabled";
-                        })
+                        button.addEventListener("click", editButtonListener);
                     });
                 }
             });
@@ -52,5 +83,4 @@
 let categoryAdmin = new WorkWithCategory();
 categoryAdmin.addEventListenerOnCreateCategory();
 categoryAdmin.addEventListenerOnEditCategory();
-
 
